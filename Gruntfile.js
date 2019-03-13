d1f0d49fdf5a716259c4291dfa0feb11079324ae
therewasaguy <jason.sigal@gmail.com>
2019-03-13 01:18:44 -0400

    uncomment Grunt requirejs tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,137 +31,137 @@ module.exports = function(grunt) {
     browserify: {
       tasks: ['browserify']
     },
-    // requirejs: {
-    //   unmin: {
-    //     options: {
-    //       baseUrl: '.',
-    //       findNestedDependencies: true,
-    //       include: ['src/app'],
-    //       onBuildWrite: function( name, path, contents ) {
-    //         if (path.indexOf('node_modules/tone/') > -1) {
-    //           return '/** Tone.js module by Yotam Mann, MIT License 2016  http://opensource.org/licenses/MIT **/\n' +
-    //           require('amdclean').clean({
-    //           'code': contents.replace(/console.log(.*);/g, ''),
-    //             'escodegen': {
-    //               'comment': false,
-    //               'skipDirOptimize':true,
-    //               'format': {
-    //                 'indent': {
-    //                   'style': '  ',
-    //                   'adjustMultiLineComment': true
-    //                 }
-    //               }
-    //             }
-    //           });
-    //         } else if (path.indexOf('node_modules/startaudiocontext') > -1) {
-    //           // return '/** StartAudioContext.js by Yotam Mann, MIT License 2017 https://github.com/tambien/StartAudioContext http://opensource.org/licenses/MIT **/\n' +
-    //           return require('amdclean').clean({
-    //             code: contents,
-    //             escodegen: {
-    //               comment: false,
-    //               format: {
-    //                 indent: {
-    //                   style: '  ',
-    //                   adjustMultiLineComment: true
-    //                 }
-    //               }
-    //             }
-    //           });
-    //         } else {
-    //           return require('amdclean').clean({
-    //             'code':contents,
-    //               'escodegen': {
-    //                 'comment': true,
-    //                 'format': {
-    //                   'indent': {
-    //                     'style': '  ',
-    //                     'adjustMultiLineComment': true
-    //                   }
-    //                 }
-    //               }
-    //             });
-    //         }
-    //       },
-    //       optimize: 'none',
-    //       out: 'lib/p5.sound.js',
-    //       paths: {
-    //         'Tone' : 'node_modules/tone/Tone',
-    //         'StartAudioContext' : 'node_modules/startaudiocontext/StartAudioContext',
-    //         'automation-timeline': 'node_modules/web-audio-automation-timeline/build/automation-timeline-amd',
-    //         'panner' : 'src/panner',
-    //         'shims': 'src/shims',
-    //         'audiocontext': 'src/audiocontext',
-    //         'master': 'src/master',
-    //         'helpers': 'src/helpers',
-    //         'errorHandler': 'src/errorHandler',
-    //         'soundfile': 'src/soundfile',
-    //         'amplitude': 'src/amplitude',
-    //         'fft': 'src/fft',
-    //         'oscillator': 'src/oscillator',
-    //         'pulse': 'src/pulse',
-    //         'noise': 'src/noise',
-    //         'audioin': 'src/audioin',
-    //         'envelope': 'src/envelope',
-    //         'delay': 'src/delay',
-    //         'effect': 'src/effect',
-    //         'panner3d' : 'src/panner3d',
-    //         'listener3d': 'src/listener3d',
-    //         'filter': 'src/filter',
-    //         'reverb': 'src/reverb',
-    //         'eq': 'src/eq',
-    //         'distortion': 'src/distortion',
-    //         'compressor': 'src/compressor',
-    //         'looper': 'src/looper',
-    //         'soundloop': 'src/soundLoop',
-    //         'soundRecorder': 'src/soundRecorder',
-    //         'signal': 'src/signal',
-    //         'metro': 'src/metro',
-    //         'peakdetect': 'src/peakDetect',
-    //         'gain': 'src/gain',
-    //         'audioVoice': 'src/audioVoice',
-    //         'monosynth': 'src/monosynth',
-    //         'polysynth': 'src/polysynth'
-    //       },
-    //       useStrict: true,
-    //       wrap: {
-    //         start: '/*! p5.sound.js v<%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n' + grunt.file.read('./fragments/before.frag'),
-    //         end: grunt.file.read('./fragments/after.frag')
-    //       }
-    //     }
-    //   },
-    //   min: {
-    //     options: {
-    //       baseUrl: '.',
-    //       findNestedDependencies: true,
-    //       include: ['src/app'],
-    //       onBuildWrite: function( name, path, contents ) {
-    //       if (path.indexOf('node_modules/tone/') > -1) {
-    //         return require('amdclean').clean({
-    //           'code':contents.replace(/console.log(.*);/g, ''),
-    //            'escodegen': {
-    //              'comment': false
-    //            }
-    //         });
-    //       } else {
-    //          return require('amdclean').clean({
-    //             'code':contents,
-    //              'escodegen': {
-    //                'comment': false
-    //              }
-    //           });
-    //        }
-    //       },
-    //       optimize: 'uglify2',
-    //       out: 'lib/p5.sound.min.js',
-    //       paths: '<%= requirejs.unmin.options.paths %>',
-    //       useStrict: true,
-    //       wrap: {
-    //         start: '/*! p5.sound.min.js v<%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n' + grunt.file.read('./fragments/before.frag'),
-    //         end: grunt.file.read('./fragments/after.frag')
-    //       }
-    //     }
-    //   },
-    // },
+    requirejs: {
+      unmin: {
+        options: {
+          baseUrl: '.',
+          findNestedDependencies: true,
+          include: ['src/app'],
+          onBuildWrite: function( name, path, contents ) {
+            if (path.indexOf('node_modules/tone/') > -1) {
+              return '/** Tone.js module by Yotam Mann, MIT License 2016  http://opensource.org/licenses/MIT **/\n' +
+              require('amdclean').clean({
+              'code': contents.replace(/console.log(.*);/g, ''),
+                'escodegen': {
+                  'comment': false,
+                  'skipDirOptimize':true,
+                  'format': {
+                    'indent': {
+                      'style': '  ',
+                      'adjustMultiLineComment': true
+                    }
+                  }
+                }
+              });
+            } else if (path.indexOf('node_modules/startaudiocontext') > -1) {
+              // return '/** StartAudioContext.js by Yotam Mann, MIT License 2017 https://github.com/tambien/StartAudioContext http://opensource.org/licenses/MIT **/\n' +
+              return require('amdclean').clean({
+                code: contents,
+                escodegen: {
+                  comment: false,
+                  format: {
+                    indent: {
+                      style: '  ',
+                      adjustMultiLineComment: true
+                    }
+                  }
+                }
+              });
+            } else {
+              return require('amdclean').clean({
+                'code':contents,
+                  'escodegen': {
+                    'comment': true,
+                    'format': {
+                      'indent': {
+                        'style': '  ',
+                        'adjustMultiLineComment': true
+                      }
+                    }
+                  }
+                });
+            }
+          },
+          optimize: 'none',
+          out: 'lib/p5.sound.js',
+          paths: {
+            'Tone' : 'node_modules/tone/Tone',
+            'StartAudioContext' : 'node_modules/startaudiocontext/StartAudioContext',
+            'automation-timeline': 'node_modules/web-audio-automation-timeline/build/automation-timeline-amd',
+            'panner' : 'src/panner',
+            'shims': 'src/shims',
+            'audiocontext': 'src/audiocontext',
+            'master': 'src/master',
+            'helpers': 'src/helpers',
+            'errorHandler': 'src/errorHandler',
+            'soundfile': 'src/soundfile',
+            'amplitude': 'src/amplitude',
+            'fft': 'src/fft',
+            'oscillator': 'src/oscillator',
+            'pulse': 'src/pulse',
+            'noise': 'src/noise',
+            'audioin': 'src/audioin',
+            'envelope': 'src/envelope',
+            'delay': 'src/delay',
+            'effect': 'src/effect',
+            'panner3d' : 'src/panner3d',
+            'listener3d': 'src/listener3d',
+            'filter': 'src/filter',
+            'reverb': 'src/reverb',
+            'eq': 'src/eq',
+            'distortion': 'src/distortion',
+            'compressor': 'src/compressor',
+            'looper': 'src/looper',
+            'soundloop': 'src/soundLoop',
+            'soundRecorder': 'src/soundRecorder',
+            'signal': 'src/signal',
+            'metro': 'src/metro',
+            'peakdetect': 'src/peakDetect',
+            'gain': 'src/gain',
+            'audioVoice': 'src/audioVoice',
+            'monosynth': 'src/monosynth',
+            'polysynth': 'src/polysynth'
+          },
+          useStrict: true,
+          wrap: {
+            start: '/*! p5.sound.js v<%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n' + grunt.file.read('./fragments/before.frag'),
+            end: grunt.file.read('./fragments/after.frag')
+          }
+        }
+      },
+      min: {
+        options: {
+          baseUrl: '.',
+          findNestedDependencies: true,
+          include: ['src/app'],
+          onBuildWrite: function( name, path, contents ) {
+          if (path.indexOf('node_modules/tone/') > -1) {
+            return require('amdclean').clean({
+              'code':contents.replace(/console.log(.*);/g, ''),
+               'escodegen': {
+                 'comment': false
+               }
+            });
+          } else {
+             return require('amdclean').clean({
+                'code':contents,
+                 'escodegen': {
+                   'comment': false
+                 }
+              });
+           }
+          },
+          optimize: 'uglify2',
+          out: 'lib/p5.sound.min.js',
+          paths: '<%= requirejs.unmin.options.paths %>',
+          useStrict: true,
+          wrap: {
+            start: '/*! p5.sound.min.js v<%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n' + grunt.file.read('./fragments/before.frag'),
+            end: grunt.file.read('./fragments/after.frag')
+          }
+        }
+      },
+    },
     open: {
       testChrome: {
         path: 'http://localhost:8000/test',
@@ -225,7 +225,7 @@ module.exports = function(grunt) {
         .bundle()
 
       const isMin = param === 'min';
-      const filename = isMin ? 'p5.sound.pre-min.js' : 'p5.sound.yo.js';
+      const filename = isMin ? 'p5.sound.pre-min.js' : 'p5.sound.babelified.js';
 
       // This file will not exist until it has been built
       const libFilePath = path.resolve('lib/' + filename);
